c5c5ff59e257bcdccf28cb6ee9021bfc1371234f
TinyKaiju <91015640+TinyKaiju@users.noreply.github.com>
2023-09-18 14:27:46 +0200

    Links

--- GADE7322POE/OpenGL/main.cpp
+++ GADE7322POE/OpenGL/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//GLFW
+#include <GLFW/glfw3.h>
+
+// SOIL2
+#include "SOIL2/SOIL2.h"
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // MAIN FUNCTION for MAIN GAME LOOP
 int main()
