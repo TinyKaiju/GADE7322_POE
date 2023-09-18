e3f1439c16b3fac336c11b2830476fe4b668c315
TinyKaiju <91015640+TinyKaiju@users.noreply.github.com>
2023-09-18 14:32:11 +0200

    Window

--- GADE7322POE/OpenGL/main.cpp
+++ GADE7322POE/OpenGL/main.cpp
@@ -19,9 +19,56 @@ using namespace std;
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+const GLint WIDTH = 1920, HEIGHT = 1080;
+int SCREEN_WIDTH, SCREEN_HEIGHT; // Replace all screenW & screenH with these
+
 // MAIN FUNCTION for MAIN GAME LOOP
 int main()
 {
+	//Initialise GLFW
+	glfwInit();
 
+	// GLFW Version Hints	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GADE7322", nullptr, nullptr);
+
+	//Get Screen Resolution
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+
+	// check if window is created succesfully
+	if (nullptr == window)
+	{
+		std::cout << "Failed to Create Window." << endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+
+	glfwMakeContextCurrent(window); //exit
+
+	//Game LOOP
+	while (!glfwWindowShouldClose(window))
+	{
+		// Set frame time
+		GLfloat currentFrame = glfwGetTime();
+
+		// Checks for events and calls corresponding response
+		glfwPollEvents();
+
+		//Render and clear the colour buffer
+		glClearColor(0.4f, 0.6f, 0.7f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		// DRAW OPENGL WINDOW/VIEWPORT
+		glfwSwapBuffers(window);
+
+	}
 }
 
