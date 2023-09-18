8767f113dc3e7aa8a854b92e224598d8683b2ea7
TinyKaiju <91015640+TinyKaiju@users.noreply.github.com>
2023-09-18 14:37:39 +0200

    Shader header

--- GADE7322POE/OpenGL/main.cpp
+++ GADE7322POE/OpenGL/main.cpp
@@ -19,6 +19,9 @@ using namespace std;
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Link Shader File
+#include "Shader.h"
+
 const GLint WIDTH = 1920, HEIGHT = 1080;
 int SCREEN_WIDTH, SCREEN_HEIGHT; // Replace all screenW & screenH with these
 
@@ -52,6 +55,154 @@ int main()
 
 	glfwMakeContextCurrent(window); //exit
 
+	#pragma region BUILD AND COMPILE SHADER - CHESSBOARD
+
+	Shader chessboardShader("CoreCB.vs", "CoreCB.frag");
+
+	// Set vertex data for our cube
+	GLfloat verticesBoard[] =
+	{
+		//Positions                //Texture Coords
+		-0.5f, 0.0f, -0.5f,        0.0f, 0.0f,
+		0.5f, 0.0f, -0.5f,        1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,        1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,        1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
+		-0.5f, 0.0f, -0.5f,        0.0f, 0.0f, // Left
+
+		-0.5f, 0.0f,  0.5f,        0.0f, 0.0f,
+		0.5f, 0.0f,  0.5f,        1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,        1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,        1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,    0.0f, 1.0f,
+		-0.5f, 0.0f,  0.5f,        0.0f, 0.0f, // Right
+
+		-0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+		-0.5f, 0.0f, -0.5f,        0.0f, 1.0f,
+		-0.5f, 0.0f, -0.5f,        0.0f, 1.0f,
+		-0.5f, 0.0f,  0.5f,        0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,    1.0f, 0.0f, // Back
+
+		0.5f,  0.5f,  0.5f,        1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,        1.0f, 1.0f,
+		0.5f, 0.0f, -0.5f,        0.0f, 1.0f,
+		0.5f, 0.0f, -0.5f,        0.0f, 1.0f,
+		0.5f, 0.0f,  0.5f,        0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,        1.0f, 0.0f, // Front
+
+		-0.5f, 0.0f, -0.5f,        0.0f, 1.0f,
+		0.5f, 0.0f, -0.5f,        1.0f, 1.0f,
+		0.5f, 0.0f,  0.5f,        1.0f, 0.0f,
+		0.5f, 0.0f,  0.5f,        1.0f, 0.0f,
+		-0.5f, 0.0f,  0.5f,        0.0f, 0.0f,
+		-0.5f, 0.0f, -0.5f,        0.0f, 1.0f, // Bottom
+
+		-0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,        1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,        1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,        1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,    0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,    0.0f, 1.0f // Top
+	};
+
+	// Positions of different cubes
+	glm::vec3 cubePositions[] =
+	{
+		glm::vec3(-3.0f, 0.0f, 4.0f),
+		glm::vec3(-2.0f, 0.0f, 4.0f),
+		glm::vec3(-1.0f, 0.0f, 4.0f),
+		glm::vec3(0.0f, 0.0f, 4.0f),
+		glm::vec3(1.0f, 0.0f, 4.0f),
+		glm::vec3(2.0f, 0.0f, 4.0f),
+		glm::vec3(3.0f, 0.0f, 4.0f),
+		glm::vec3(4.0f, 0.0f, 4.0f)
+	};
+
+	// Generate the vertex arrays and vertex buffers and save them into variables
+	GLuint VBA_Board, VOA_Board;
+	glGenVertexArrays(1, &VOA_Board);
+	glGenBuffers(1, &VBA_Board);
+
+	// Bind the vertex array object
+	glBindVertexArray(VOA_Board);
+
+	// Bind and set the vertex buffers
+	glBindBuffer(GL_ARRAY_BUFFER, VBA_Board);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesBoard), verticesBoard, GL_STATIC_DRAW);
+
+	// Create the vertex pointer and enable the vertex array
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0); //Position
+	glEnableVertexAttribArray(0);
+
+	// Texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat))); //Texture
+	glEnableVertexAttribArray(2);
+
+	// Unbind the vertex array to prevent strange bugs
+	glBindVertexArray(0);
+
+#pragma region Chessboard Texture
+
+	// Chessboard texture variables
+	GLuint textureWhite, textureBlack;
+	int widthB, heightB;
+#pragma endregion
+
+#pragma region White Texture
+
+	// Create and load White texture
+	glGenTextures(1, &textureWhite);
+	glBindTexture(GL_TEXTURE_2D, textureWhite);
+
+	// Set texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Actual texture loading code
+	unsigned char* whiteBlock = SOIL_load_image("res/images/white.jpg", &widthB, &heightB, 0, SOIL_LOAD_RGBA);
+
+	// Specify 2D texture image
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthB, heightB, 0, GL_RGBA, GL_UNSIGNED_BYTE, whiteBlock);
+
+	// Generate mipmaps
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(whiteBlock);
+	glBindTexture(GL_TEXTURE_2D, 0);
+#pragma endregion
+
+#pragma region Black Texture
+
+	//Create Black texture
+	glGenTextures(1, &textureBlack);
+	glBindTexture(GL_TEXTURE_2D, textureBlack);
+
+	//Set texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	//Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//Load texture
+	unsigned char* blackBlock = SOIL_load_image("res/images/black.jpg", &widthB, &heightB, 0, SOIL_LOAD_RGBA);
+
+	//Specify 2D texture image
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthB, heightB, 0, GL_RGBA, GL_UNSIGNED_BYTE, blackBlock);
+
+	// Generate mipmaps
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(blackBlock);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+#pragma endregion
+
+#pragma endregion
 	//Game LOOP
 	while (!glfwWindowShouldClose(window))
 	{
